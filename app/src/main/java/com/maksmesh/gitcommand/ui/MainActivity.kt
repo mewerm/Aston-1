0f98a87388b8a6975f4acba67b9a689d6d2a6a26
Maxim Meshcheryakov <25-nv-86@mail.ru>
2022-11-03 17:30:18 +0600

    just added the activity

--- app/src/main/java/com/maksmesh/gitcommand/ui/MainActivity.kt
+++ app/src/main/java/com/maksmesh/gitcommand/ui/MainActivity.kt
@@ -1,4 +1,13 @@
 package com.maksmesh.gitcommand.ui
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.maksmesh.gitcommand.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
